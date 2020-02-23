bac93c6a4882246e9c75bfc5a582df5612ea99e5
Mike Lodato <zvxryb@gmail.com>
2020-02-23 02:46:14 -0500

    factor out text input element; add type hints to console_ui.py

--- console_ui.py
+++ console_ui.py
@@ -16,6 +16,9 @@
 import codecs, ctypes, msvcrt, re, sys, unicodedata
 from ctypes.wintypes import DWORD
 from textwrap import TextWrapper
+from typing import Any, Callable, List, Optional, Tuple, Union
+
+Box = Tuple[int, int, int, int]
 
 from util import char_width, unicode_width, unicode_ljust, unicode_center, line_break_opportunities
 
@@ -37,6 +40,9 @@ class WinAnsiMode:
     def __exit__(self, type, value, tb):
         kernel32.SetConsoleMode(self.outdev, self.mode)
 
+def getwch() -> str:
+    return msvcrt.getwch()
+
 ANSI_CLEAR   = '\033[H\033[J'
 ANSI_RESET   = '\033[0m'
 ANSI_UP      = '\033[A'
@@ -51,10 +57,10 @@ ANSI_WHITE   = '\033[37m'
 ANSI_SAVE    = '\033[s'
 ANSI_RESTORE = '\033[u'
 
-def ansi_pos(row, col):
+def ansi_pos(row: int, col: int) -> str:
     return f'\033[{row};{col}H'
 
-def ansi_col(col):
+def ansi_col(col: int) -> str:
     return f'\033[{col}G'
 
 ASCII_BACKSPACE = '\x08'
@@ -68,8 +74,11 @@ MS_KEY_DOWN  = '\x50'
 MS_KEY_HOME  = '\x47'
 MS_KEY_END   = '\x4f'
 
-RESULT_PASS = 0
-RESULT_FAIL = 1
+class TestResult:
+    __slots__ = ()
+
+RESULT_PASS = TestResult()
+RESULT_FAIL = TestResult()
 
 class Formatter:
     __slots__ = (
@@ -84,7 +93,17 @@ class Formatter:
         '__modified',
         '__lines')
 
-    def __init__(self, row, col, rows, cols, center_h, center_v, style=None):
+    __lines: List[Tuple[int, int, int]]
+
+    def __init__(self,
+        row: int,
+        col: int,
+        rows: int,
+        cols: int,
+        center_h: bool,
+        center_v: bool,
+        style: str = ''
+    ):
         self.__row = row
         self.__col = col
         self.__rows = rows
@@ -95,24 +114,24 @@ class Formatter:
         self.text = ''
 
     @property
-    def text(self):
+    def text(self) -> str:
         return self.__text
 
     @text.setter
-    def text(self, text):
-        self.__text = text or ''
+    def text(self, text: str):
+        self.__text = text
         self.__modified = True
 
     @property
-    def style(self):
+    def style(self) -> str:
         return self.__style
 
     @style.setter
-    def style(self, style):
+    def style(self, style: str):
         self.__style = style
 
     @property
-    def lines(self):
+    def lines(self) -> List[Tuple[int, int, int]]:
         if not self.__modified:
             return self.__lines
 
@@ -153,7 +172,7 @@ class Formatter:
 
         return self.__lines
 
-    def local_cursor_pos(self, index):
+    def local_cursor_pos(self, index: int) -> Tuple[int, int, int]:
         line, char, line_cols = 0, 0, 0
         for i, (start, end, width) in enumerate(self.lines):
             line = i
@@ -164,7 +183,7 @@ class Formatter:
             char = width
         return line, char, line_cols
 
-    def global_cursor_pos(self, index):
+    def global_cursor_pos(self, index: int) -> Tuple[int, int]:
         n = len(self.lines)
         row, col, m = self.local_cursor_pos(index)
 
@@ -190,10 +209,7 @@ class Formatter:
         return out_row, out_col
 
     def redraw(self):
-        out = ANSI_SAVE
-
-        if self.style:
-            out += self.style
+        out = ANSI_SAVE + self.style
 
         n = len(self.lines)
 
@@ -225,16 +241,38 @@ class Formatter:
         out += ANSI_RESET + ANSI_RESTORE
         sys.stdout.write(out)
 
+class HistoryData:
+    __slots__ = 'id', 'modified', 'result', 'question', 'expected', 'answered'
+
+    def __init__(self,
+        id: Any = None,
+        result: Optional[TestResult] = None,
+        question: str = '',
+        expected: str = '',
+        answered: str = ''
+    ):
+        self.id       = id
+        self.modified = False
+        self.result   = result
+        self.question = question
+        self.expected = expected
+        self.answered = answered
+
 class HistoryForm:
     __slots__ = 'result', 'question', 'expected', 'answered'
 
-    def __init__(self, result, question, expected, answered):
+    def __init__(self,
+        result  : Box,
+        question: Box,
+        expected: Box,
+        answered: Box
+    ):
         self.result   = Formatter(*result  , True , True )
         self.question = Formatter(*question, False, False)
         self.expected = Formatter(*expected, False, False)
         self.answered = Formatter(*answered, False, False)
 
-    def update_result(self, result, highlight):
+    def update_result(self, result: Optional[TestResult], highlight: bool):
         if result is None:
             self.result.style = ''
             self.result.text  = ''
@@ -250,19 +288,19 @@ class HistoryForm:
 
         self.result.redraw()
 
-    def update_question(self, text):
+    def update_question(self, text: str):
         self.question.text = text
         self.question.redraw()
 
-    def update_expected(self, text):
+    def update_expected(self, text: str):
         self.expected.text = text
         self.expected.redraw()
 
-    def update_answered(self, text):
+    def update_answered(self, text: str):
         self.answered.text = text
         self.answered.redraw()
 
-    def update(self, data, highlight):
+    def update(self, data: HistoryData, highlight: bool):
         self.update_result(data.result, highlight)
         self.update_question((f'{data.id}: ' + data.question) if data.question else '')
         self.update_expected(data.expected)
@@ -278,24 +316,13 @@ NUMBER_BOX   = (22, 3, 1,   2)
 TOTAL_BOX    = (22, 6, 1,   2)
 ANSWER_BOX   = (27, 3, 2, 115)
 
-class HistoryData:
-    __slots__ = 'id', 'modified', 'result', 'question', 'expected', 'answered'
-
-    def __init__(self, id=None, result=None, question=None, expected=None, answered=None):
-        self.id       = id
-        self.modified = False
-        self.result   = result
-        self.question = question
-        self.expected = expected
-        self.answered = answered
-
 class Display:
-    def __init__(self, on_submit, on_revise):
-        self.input     = u''
-        self.cursor    = 0
+    selected: Optional[int]
+
+    def __init__(self, on_submit: Callable[[str], None], on_revise: Callable[[Any, TestResult], None]):
+        self.answer    = Input(ANSWER_BOX)
         self.history   = [HistoryData() for _ in HISTORY_FORMS]
         self.question_formatter = Formatter(*QUESTION_BOX, True , True )
-        self.answer_formatter   = Formatter(*ANSWER_BOX  , False, False)
         self.selected  = None
         self.on_submit = on_submit
         self.on_revise = on_revise
@@ -320,12 +347,12 @@ class Display:
         self.history = [HistoryData() for _ in HISTORY_FORMS]
         self.redraw_history()
 
-    def push_history(self, id, result, question, expected, answered):
+    def push_history(self, id: Any, result: TestResult, question: str, expected: str, answered: str):
         item = HistoryData(id, result, question, expected, answered)
         self.history = [item] + self.history[:len(HISTORY_FORMS)-1]
         self.redraw_history()
 
-    def update_question(self, question, number, total):
+    def update_question(self, question: str, number: Union[int, str], total: Union[int, str]):
         number = str(number)
         if len(number) > NUMBER_BOX[3]:
             number = '#' * NUMBER_BOX[3]
@@ -347,11 +374,12 @@ class Display:
         self.question_formatter.text = question
         self.question_formatter.redraw()
 
-    def set_selected(self, selected):
+    def set_selected(self, selected: Optional[int]):
         if self.selected is not None:
             history_data = self.history[self.selected]
             if history_data.modified:
                 history_data.modified = False
+                assert history_data.result is not None
                 self.on_revise(history_data.id, history_data.result)
             HISTORY_FORMS[self.selected].update_result(history_data.result, False)
 
@@ -361,7 +389,7 @@ class Display:
             history_data = self.history[self.selected]
             HISTORY_FORMS[self.selected].update_result(history_data.result, True)
 
-    def select_item(self, up):
+    def select_item(self, up: bool):
         n = len(HISTORY_FORMS)
         if up:
             if self.selected is None:
@@ -391,35 +419,72 @@ class Display:
         history_data.modified = True
         HISTORY_FORMS[self.selected].update_result(history_data.result, True)
 
+    def main(self) -> int:
+        while True:
+            if self.selected is None:
+                event_or_input = self.answer.get_input()
+                if isinstance(event_or_input, str):
+                    self.on_submit(event_or_input)
+                elif event_or_input == Input.UNFOCUS:
+                    return 0
+                elif event_or_input == Input.KEY_UP:
+                    self.select_item(True)
+                elif event_or_input == Input.KEY_DOWN:
+                    self.select_item(False)
+            else:
+                char = getwch()
+                if char == ASCII_ESC:
+                    self.set_selected(None)
+                elif char == '\x03' or char == '\x04':
+                    raise KeyboardInterrupt()
+                elif char == MS_KEY_ESC0 or char == MS_KEY_ESC1:
+                    char2 = getwch()
+                    if char2 == MS_KEY_UP or char2 == MS_KEY_DOWN:
+                        self.select_item(char2 == MS_KEY_UP)
+                elif char == ' ':
+                    self.toggle_item()
+                    self.set_selected(None)
+            sys.stdout.flush()
+
+class Input:
+    class Event:
+        __slots__ = ()
+
+    UNFOCUS  = Event()
+    KEY_UP   = Event()
+    KEY_DOWN = Event()
+
+    def __init__(self, box: Box):
+        self.input     = u''
+        self.cursor    = 0
+        self.formatter = Formatter(*box, False, False)
+
     def update_cursor(self):
-        self.answer_formatter.text = self.input
-        row, col = self.answer_formatter.global_cursor_pos(self.cursor)
+        self.formatter.text = self.input
+        row, col = self.formatter.global_cursor_pos(self.cursor)
         sys.stdout.write(ansi_pos(row, col))
 
     def redraw_input(self):
-        self.answer_formatter.text = self.input
-        self.answer_formatter.redraw()
+        self.formatter.text = self.input
+        self.formatter.redraw()
 
-    def main(self):
+    def get_input(self) -> Union[Event, str]:
         while True:
-            char = msvcrt.getwch()
+            char = getwch()
             if char == ASCII_ESC:
-                if self.selected is None:
-                    return 0
-                else:
-                    self.set_selected(None)
+                return Input.UNFOCUS
             elif char == '\x03' or char == '\x04':
-                return 0
+                raise KeyboardInterrupt()
             elif char == '\n':
                 pass
             elif char == '\r':
-                if self.selected is None:
-                    if not self.on_submit(self.input):
-                        return 0
-                    self.input = ''
-                    self.cursor = 0
-                    self.update_cursor()
-                    self.redraw_input()
+                result = self.input
+                self.input = ''
+                self.cursor = 0
+                self.update_cursor()
+                self.redraw_input()
+                sys.stdout.flush()
+                return result
             elif char == ASCII_BACKSPACE:
                 if self.cursor > 0:
                     self.cursor -= 1
@@ -427,30 +492,27 @@ class Display:
                     self.update_cursor()
                     self.redraw_input()
             elif char == MS_KEY_ESC0 or char == MS_KEY_ESC1:
-                char2 = msvcrt.getwch()
-                if char2 == MS_KEY_UP or char2 == MS_KEY_DOWN:
-                    self.select_item(char2 == MS_KEY_UP)
+                char2 = getwch()
+                if char2 == MS_KEY_UP:
+                    return Input.KEY_UP
+                elif char2 == MS_KEY_DOWN:
+                    return Input.KEY_DOWN
                 elif char2 == MS_KEY_LEFT or char2 == MS_KEY_RIGHT:
-                    if self.selected is None:
-                        if char2 == MS_KEY_LEFT:
-                            if self.cursor > 0:
-                                self.cursor -= 1
-                                self.update_cursor()
-                        else:
-                            if self.cursor < len(self.input):
-                                self.cursor += 1
-                                self.update_cursor()
+                    if char2 == MS_KEY_LEFT:
+                        if self.cursor > 0:
+                            self.cursor -= 1
+                            self.update_cursor()
                     else:
-                        self.toggle_item()
+                        if self.cursor < len(self.input):
+                            self.cursor += 1
+                            self.update_cursor()
                 elif char2 == MS_KEY_HOME:
-                    if self.selected is None:
-                        self.cursor = 0
-                        self.update_cursor()
+                    self.cursor = 0
+                    self.update_cursor()
                 elif char2 == MS_KEY_END:
-                    if self.selected is None:
-                        self.cursor = len(self.input)
-                        self.update_cursor()
-            elif self.selected is None:
+                    self.cursor = len(self.input)
+                    self.update_cursor()
+            else:
                 if self.cursor < len(self.input):
                     self.input = self.input[:self.cursor] + char + self.input[self.cursor:]
                 else:
@@ -458,9 +520,6 @@ class Display:
                 self.cursor += 1
                 self.update_cursor()
                 self.redraw_input()
-            elif char == ' ':
-                self.toggle_item()
-                self.set_selected(None)
             sys.stdout.flush()
 
 if __name__ == '__main__':
