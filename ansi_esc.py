9df358bd42d5fa3fc21f4aadbecc68ecb3cfeaaa
Mike Lodato <zvxryb@gmail.com>
2020-02-28 00:59:08 -0500

    implement functions; add 'fgcolor' function

--- ansi_esc.py
+++ ansi_esc.py
@@ -1,19 +1,35 @@
+def ansi_sgr(code: int) -> str:
+    return f'\033[{code}m'
+
 ANSI_CLEAR   = '\033[H\033[J'
-ANSI_RESET   = '\033[0m'
+ANSI_RESET   = ansi_sgr(0)
 ANSI_UP      = '\033[A'
 ANSI_DOWN    = '\033[B'
 ANSI_FORWARD = '\033[C'
 ANSI_BACK    = '\033[D'
-ANSI_REVERSE = '\033[7m'
-ANSI_RED     = '\033[31m'
-ANSI_GREEN   = '\033[32m'
-ANSI_CYAN    = '\033[36m'
-ANSI_WHITE   = '\033[37m'
+ANSI_REVERSE = ansi_sgr(7)
+ANSI_BLACK   = ansi_sgr(30)
+ANSI_RED     = ansi_sgr(31)
+ANSI_GREEN   = ansi_sgr(32)
+ANSI_YELLOW  = ansi_sgr(33)
+ANSI_BLUE    = ansi_sgr(34)
+ANSI_MAGENTA = ansi_sgr(35)
+ANSI_CYAN    = ansi_sgr(36)
+ANSI_WHITE   = ansi_sgr(37)
+ANSI_DEFAULT = ansi_sgr(39)
+ANSI_BRIGHT_BLACK   = ansi_sgr(90)
+ANSI_BRIGHT_RED     = ansi_sgr(91)
+ANSI_BRIGHT_GREEN   = ansi_sgr(92)
+ANSI_BRIGHT_YELLOW  = ansi_sgr(93)
+ANSI_BRIGHT_BLUE    = ansi_sgr(94)
+ANSI_BRIGHT_MAGENTA = ansi_sgr(95)
+ANSI_BRIGHT_CYAN    = ansi_sgr(96)
+ANSI_BRIGHT_WHITE   = ansi_sgr(97)
 ANSI_SAVE    = '\033[s'
 ANSI_RESTORE = '\033[u'
 
-def ansi_pos(row, col):
+def ansi_pos(row: int, col: int) -> str:
     return f'\033[{row};{col}H'
 
-def ansi_col(col):
+def ansi_col(col: int) -> str:
     return f'\033[{col}G'
--- markup.py
+++ markup.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-import logging, sys, unicodedata
+import abc, inspect, logging, sys, threading, unicodedata
 from functools import reduce
-from typing import Callable, Dict, Generator, Iterator, List, Optional, Tuple, Union
+from typing import Any, Callable, Dict, Generator, Iterator, List, Optional, Tuple, Union
 
 from ansi_esc import *
 from console_ui import WinAnsiMode
@@ -34,8 +34,10 @@ class Token:
             return self.ident
 
     LITERAL  = Type('literal')
+    ESCAPE   = Type('escape')
     SPACE    = Type('whitespace')
     INFIX    = Type('infix')
+    FUNCTION = Type('function')
     LBRACKET = Type('lbracket')
     RBRACKET = Type('rbracket')
 
@@ -60,8 +62,6 @@ def tokenize(s: str) -> Generator[Token, None, None]:
         yield Token(Token.LITERAL, (i, j), s[i:])
 
 def normalize(tokens: Iterator[Token]) -> Generator[Token, None, None]:
-    global INFIX_OPERATORS
-
     t0: Optional[Token] = None
 
     while True:
@@ -70,7 +70,16 @@ def normalize(tokens: Iterator[Token]) -> Generator[Token, None, None]:
         except StopIteration:
             break
 
-        if t1.value in INFIX_OPERATORS:
+        if t0 and t0.type == Token.ESCAPE:
+            if t1.value in Function.LOOKUP:
+                t1.type = Token.FUNCTION
+            t0 = t1
+        elif t1.value == '\\':
+            t1.type = Token.ESCAPE
+            if t0:
+                yield t0
+            t0 = t1
+        elif t1.value in InfixOperator.LOOKUP:
             t1.type = Token.INFIX
             if t0:
                 if t0.type == Token.SPACE:
@@ -82,7 +91,7 @@ def normalize(tokens: Iterator[Token]) -> Generator[Token, None, None]:
             t1.type = Token.LBRACKET
             if t0:
                 yield t0
-                if t0.type in (Token.LITERAL, Token.RBRACKET):
+                if t0.type in (Token.LITERAL, Token.FUNCTION, Token.RBRACKET):
                     yield Token(Token.SPACE, None, '')
             t0 = t1
         elif t1.value == '}':
@@ -97,7 +106,7 @@ def normalize(tokens: Iterator[Token]) -> Generator[Token, None, None]:
             if t0 and t0.type == Token.SPACE:
                 t0.value += t1.value
             else:
-                if t0 and t0.type == Token.LITERAL:
+                if t0 and t0.type in (Token.LITERAL, Token.FUNCTION, Token.RBRACKET):
                     t1.type = Token.SPACE
                 else:
                     t1.type = Token.LITERAL
@@ -108,7 +117,7 @@ def normalize(tokens: Iterator[Token]) -> Generator[Token, None, None]:
             t1.type = Token.LITERAL
             if t0:
                 yield t0
-                if t0.type in (Token.LITERAL, Token.RBRACKET):
+                if t0.type in (Token.LITERAL, Token.FUNCTION, Token.RBRACKET):
                     yield Token(Token.SPACE, None, '')
             t0 = t1
 
@@ -144,6 +153,10 @@ class TextBox:
     @staticmethod
     def from_str(text: str) -> TextBox:
         return TextBox(unicode_width(text), 1, 0)
+
+    @staticmethod
+    def empty() -> TextBox:
+        return TextBox(0, 0, 0)
 
 class TextGroup:
     __slots__ = 'box', 'items'
@@ -194,7 +207,7 @@ class TextGroup:
 
     @staticmethod
     def empty() -> TextGroup:
-        return TextGroup(TextBox(0, 0, 0), [])
+        return TextGroup(TextBox.empty(), [])
 
 class Template:
     __slots__ = 'argn', 'definition'
@@ -226,31 +239,127 @@ class Template:
             else:
                 yield args[group]
 
-class InfixOperator:
-    __slots__ = 'symbol', 'precedence', 'math_mode', 'evaluate'
+def with_tls(f: Callable[..., Any]) -> Callable[..., Any]:
+    tls = threading.local()
+    def g(*args: Any, **kwargs: Any):
+        return f(tls, *args, **kwargs)
+    return g
+
+class Function:
+    PushArgType = Callable[[Optional[TextGroup]], Tuple[List[str], Optional[TextGroup]]]
+
+    LOOKUP: Dict[str, Function] = {}
+
+    __slots__ = 'math_mode', 'ident', 'push_arg'
 
     def __init__(self,
-        symbol: str,
-        precedence: int,
         math_mode: bool,
-        evaluate: Callable[[TextGroup, TextGroup], TextGroup]
+        ident: str,
+        push_arg: Function.PushArgType
     ):
-        self.symbol = symbol
-        self.precedence = precedence
         self.math_mode = math_mode
-        self.evaluate = evaluate
+        self.ident     = ident
+        self.push_arg  = push_arg
 
-INFIX_OPERATORS: Dict[str, InfixOperator] = {}
-def infix_operator(symbol: str, precedence: int, math_mode: bool
-) -> Callable[[Callable[[TextGroup, TextGroup], TextGroup]], InfixOperator]:
-    def f(g: Callable[[TextGroup, TextGroup], TextGroup]) -> InfixOperator:
-        global INFIX_OPERATORS
-        operator = InfixOperator(symbol, precedence, math_mode, g)
-        INFIX_OPERATORS[symbol] = operator
-        return operator
-    return f
+    @staticmethod
+    def define(math_mode: bool, ident: str = '') -> Callable[[Function.PushArgType], Function]:
+        def f(push_arg: Function.PushArgType) -> Function:
+            nonlocal ident
+            if not ident:
+                ident = push_arg.__name__
+            function = Function(math_mode, ident, push_arg)
+            Function.LOOKUP[ident] = function
+            return function
+        return f
 
-@infix_operator('^', 10, False)
+FG_COLORS = {
+    'black'        : ANSI_BLACK,
+    'red'          : ANSI_RED,
+    'green'        : ANSI_GREEN,
+    'yellow'       : ANSI_YELLOW,
+    'blue'         : ANSI_BLUE,
+    'magenta'      : ANSI_MAGENTA,
+    'cyan'         : ANSI_CYAN,
+    'lightgray'    : ANSI_WHITE,
+    'darkgray'     : ANSI_BRIGHT_BLACK,
+    'brightred'    : ANSI_BRIGHT_RED,
+    'brightgreen'  : ANSI_BRIGHT_GREEN,
+    'brightyellow' : ANSI_BRIGHT_YELLOW,
+    'brightblue'   : ANSI_BRIGHT_BLUE,
+    'brightmagenta': ANSI_BRIGHT_MAGENTA,
+    'brightcyan'   : ANSI_BRIGHT_CYAN,
+    'white'        : ANSI_BRIGHT_WHITE
+}
+
+@Function.define(False, 'fgcolor')
+@with_tls
+def fgcolor(tls, arg: TextGroup) -> Tuple[List[str], Optional[TextGroup]]:
+    try:
+        state = tls.state
+    except AttributeError:
+        state = [ANSI_DEFAULT]
+        tls.state = state
+
+    if arg is None:
+        state += [None]
+        return [], None
+
+    if state[-1] is None:
+        for item in arg.items:
+            key = item.text.strip().lower()
+            try:
+                color = FG_COLORS[key]
+            except KeyError:
+                continue
+            state[-1] = color
+            return [], None
+        state[-1] = ANSI_DEFAULT
+        return ['failed to get color'], None
+
+    prefix = state.pop()
+    suffix = state[-1]
+    return [], TextGroup(arg.box, [Text.from_str(prefix), *arg.items, Text.from_str(suffix)])
+
+class InfixOperator:
+    class Associativity:
+        __slots__ = ()
+
+    LEFT  = Associativity()
+    RIGHT = Associativity()
+
+    EvaluateType = Callable[[TextGroup, TextGroup], TextGroup]
+
+    LOOKUP: Dict[str, InfixOperator] = {}
+
+    __slots__ = 'math_mode', 'symbol', 'precedence', 'associativity', 'evaluate'
+
+    def __init__(self,
+        math_mode    : bool,
+        symbol       : str,
+        precedence   : int,
+        associativity: Associativity,
+        evaluate     : InfixOperator.EvaluateType
+    ):
+        self.math_mode     = math_mode
+        self.symbol        = symbol
+        self.precedence    = precedence
+        self.associativity = associativity
+        self.evaluate      = evaluate
+
+    @staticmethod
+    def define(
+        math_mode    : bool,
+        symbol       : str,
+        precedence   : int,
+        associativity: Associativity
+    ) -> Callable[[InfixOperator.EvaluateType], InfixOperator]:
+        def f(evaluate: InfixOperator.EvaluateType) -> InfixOperator:
+            operator = InfixOperator(math_mode, symbol, precedence, associativity, evaluate)
+            InfixOperator.LOOKUP[symbol] = operator
+            return operator
+        return f
+
+@InfixOperator.define(False, '^', 10, InfixOperator.RIGHT)
 def infix_text_over(lhs: TextGroup, rhs: TextGroup) -> TextGroup:
     width = max(lhs.box.width, rhs.box.width)
     lhs_pad = (width - lhs.box.width) // 2
@@ -259,7 +368,7 @@ def infix_text_over(lhs: TextGroup, rhs: TextGroup) -> TextGroup:
     y_offset = lhs.box.height
     return lhs.concat(rhs, x_offset = x_offset, y_offset = y_offset)
 
-@infix_operator('_', 10, False)
+@InfixOperator.define(False, '_', 10, InfixOperator.RIGHT)
 def infix_text_under(lhs: TextGroup, rhs: TextGroup) -> TextGroup:
     width = max(lhs.box.width, rhs.box.width)
     lhs_pad = (width - lhs.box.width) // 2
@@ -273,14 +382,29 @@ def layout(tokens: Iterator[Token]) -> Tuple[List[Tuple[str, Optional[Tuple[int,
     LOG.info('beginning parsing/layout')
 
     output: List[TextGroup] = []
-    operators: List[Tuple[int, Token]] = []
+    operators: List[Token] = []
     quirks: List[Tuple[str, Optional[Tuple[int, int]]]] = []
+
+    def push_arg(token: Token, arg: Optional[TextGroup]):
+        nonlocal output, operators, quirks
+        LOG.debug(f'push_arg {token} {arg}')
+        quirks_, result = Function.LOOKUP[token.value].push_arg(arg)
+        if quirks_:
+            quirks += [(quirk, token.range) for quirk in quirks_]
+        if result:
+            LOG.debug(f'\tresult {result}')
+            output += [result]
+        else:
+            operators += [token]
 
     def evaluate(token: Token):
         nonlocal output, quirks
         LOG.debug(f'eval {token}')
-        if token.type == Token.INFIX:
-            op = INFIX_OPERATORS[token.value]
+        if token.type == Token.FUNCTION:
+            quirks += [('missing argument for function', token.range)]
+            push_arg(token, TextGroup.empty())
+        elif token.type == Token.INFIX:
+            op = InfixOperator.LOOKUP[token.value]
             rhs = TextGroup.empty()
             lhs = TextGroup.empty()
             try:
@@ -311,42 +435,72 @@ def layout(tokens: Iterator[Token]) -> Tuple[List[Tuple[str, Optional[Tuple[int,
         else:
             assert False
 
-    for token in tokens:
+    def process_input(token: Token):
+        nonlocal output, operators, quirks
         LOG.debug('-'*80)
         LOG.debug(f'input {token}')
-        if token.type == Token.LBRACKET:
+        if token.type == Token.FUNCTION:
+            push_arg(token, None)
+        elif token.type == Token.LBRACKET:
             assert token.value == '{'
-            operators += [(0, token)]
+            operators += [token]
         elif token.type == Token.RBRACKET:
+            assert token.value == '}'
             while True:
                 try:
-                    _, token_ = operators.pop()
+                    token_ = operators.pop()
                 except IndexError:
                     quirks += [('unmatched right brace', token.range)]
                     break
-                if token_.value == '{':
+                if token_.type == Token.LBRACKET:
+                    assert token_.value == '{'
                     break
                 else:
                     evaluate(token_)
+            if operators and operators[-1].type == Token.FUNCTION:
+                token_ = operators.pop()
+                arg = output.pop()
+                push_arg(token_, arg)
         elif token.type == Token.INFIX:
-            op = INFIX_OPERATORS[token.value]
-            while operators and op.precedence <= operators[-1][0]:
-                _, token_ = operators.pop()
+            op = InfixOperator.LOOKUP[token.value]
+            while operators:
+                token_ = operators[-1]
+                try:
+                    op_ = InfixOperator.LOOKUP[token_.value]
+                except KeyError:
+                    break
+                if op.precedence > op_.precedence:
+                    break
+                if op.associativity == InfixOperator.RIGHT and op.precedence >= op_.precedence:
+                    break
+                operators = operators[:-1]
                 evaluate(token_)
-            operators += [(op.precedence, token)]
+            operators += [token]
         elif token.type == Token.SPACE:
-            while operators and operators[-1][1].type != Token.LBRACKET:
-                _, token_ = operators.pop()
-                evaluate(token_)
-            operators += [(1, token)]
+            if operators and operators[-1].type == Token.FUNCTION:
+                pass
+            else:
+                while operators and operators[-1].type != Token.LBRACKET:
+                    token_ = operators.pop()
+                    evaluate(token_)
+                operators += [token]
         else:
-            output += [TextGroup.from_str(token.value)]
+            text = TextGroup.from_str(token.value)
+            if operators and operators[-1].type == Token.FUNCTION:
+                token_ = operators.pop()
+                push_arg(token_, text)
+            else:
+                output += [text]
         LOG.debug(f'output state {output}')
         LOG.debug(f'operators state {operators}')
 
+    for token in tokens:
+        process_input(token)
+
     while operators:
-        _, token = operators.pop()
-        if token.value == '{':
+        token = operators.pop()
+        if token.type == Token.LBRACKET:
+            assert token.value == '{'
             quirks += [('unmatched left brace', token.range)]
         else:
             evaluate(token)
@@ -374,7 +528,7 @@ with WinAnsiMode():
 
     sys.stdout.write(ANSI_SAVE + ANSI_CLEAR)
     for item in group.items:
-        sys.stdout.write(ANSI_SAVE + ansi_pos(10 - item.y, item.x + 1) + item.text + ANSI_RESTORE)
+        sys.stdout.write(ansi_pos(10 - item.y, item.x + 1) + item.text)
     sys.stdout.write(ANSI_RESTORE)
 
     sys.stdout.flush()
